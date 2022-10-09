9d59cdccb4082d4f69acdc0b675ab7154d43bf1f
Ilya Tituchenko <titu4enko@mail.ru>
2022-10-09 20:17:51 +0300

    Ex 52 - get Arg of each column - solved

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ PrintArray(Array);
 
 
 */
-
+/*
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
 //Например, задан массив:
@@ -48,6 +48,60 @@ Console.Write("j=");
 int j=int.Parse(Console.ReadLine()!);
 
 printIJ(Array2,i,j);
+
+
+*/
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+Console.WriteLine ("Введите количество строк: ");
+int rows=int.Parse(Console.ReadLine()!);
+
+Console.WriteLine ("Введите количество столбцов: ");
+int columns=int.Parse(Console.ReadLine()!);
+
+int [,] Array3= GetArray(rows,columns,0,9);
+PrintArray(Array3);
+
+Console.WriteLine ($"Среднее арифметическое каждого столбца: {JoinAndRound(GetArgColumn(Array3),';')}.");
+
+string JoinAndRound (double [] arr,char razdel) // Сцепим строку красиво с округлением, String.Join так не умеет
+{
+    string result="";
+    for (int i=0; i<arr.Length;i++){
+        if (i!=arr.Length-1) result+=$"{arr[i]:F1}{razdel}";
+        else result+=$"{arr[i]:F1}";
+    }
+    return result;
+
+}
+
+double[] GetArgColumn (int [,] arr) // Double, а иначе целочисленно будет
+{
+    double[] summOfCol = new double[arr.GetLength(1)]; // создадим массив длинной = кол-ву столбцов в основном массиве
+    for (int j=0; j<arr.GetLength(1);j++) // Вначале столбцы, потом строки!
+    {
+        int tempSum=0; //сложим суда сумму по строкам в конкретном столбце
+        for (int i=0;i<arr.GetLength(0);i++)
+        {
+          tempSum+=arr[i,j];
+        }
+        summOfCol[j]=Convert.ToDouble(tempSum)/Convert.ToDouble(arr.GetLength(0)); // Можно было поделить на i - на выходе оно максимально, но так понятнее
+        
+    }
+    return summOfCol;
+}
+
+
+
 
 void printIJ (int [,] arr, int i, int j)
 {
